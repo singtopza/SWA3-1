e284484d2aa0802e31fa15a889da95ed8b4e9e9f
singtopza <59428798+singtopza@users.noreply.github.com>
2021-07-19 15:18:03 +0700

    Initial commit

---
+++ src/ObjectPassDemo.java
@@ -0,0 +1,20 @@
+// Java program to demonstrate objects
+// passing to methods.
+class ObjectPassDemo
+{
+    int a, b;
+  
+    ObjectPassDemo(int i, int j)
+    {
+        a = i;
+        b = j;
+    }
+  
+    // return true if o is equal to the invoking
+    // object notice an object is passed as an
+    // argument to method
+    boolean equalTo(ObjectPassDemo o)
+    {
+        return (o.a == a && o.b == b);
+    }
+}
